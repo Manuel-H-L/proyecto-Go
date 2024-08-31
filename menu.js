412b32828ab7c510751e47629e8c856fae3aba51
Manuel-H-L <99635124+Manuel-H-L@users.noreply.github.com>
2024-08-31 14:22:03 -0600

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,95 @@
+const logo = document.querySelector('.nav__logo');
+const close = document.querySelector('.nav__close');
+const titulo = document.querySelector('.hero__title');
+const imagenes = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.todos');
+const btnEnsaladas = document.querySelector('.ensaladas');
+const btnPasta = document.querySelector('.pasta');
+const btnPizza = document.querySelector('.pizza');
+const btnPostres = document.querySelector('.postres');
+const contenedorPlatillos = document.querySelector('.platillos');
+document.addEventListener('DOMContentLoaded',()=>{
+    platillos();
+    eventos();
+});
+
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    
+
+
+})();
+
+const observer = new IntersectionObserver((entries, observer)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            const imagen = entry.target;
+            imagen.src = imagen.dataset.src;
+            observer.unobserve(imagen);
+        }
+    }); 
+});
+
+
+imagenes.forEach(imagen=>{
+
+observer.observe(imagen);
+});
+
+
+const platillos = () =>{
+    let platillosArreglo = [];
+    const platillos = document.querySelectorAll('.platillo');
+
+    platillos.forEach(platillo=> platillosArreglo = [...platillosArreglo,platillo]);
+
+    const ensaladas = platillosArreglo.filter(ensalada=> ensalada.getAttribute('data-platillo') === 'ensalada');
+    const pastas = platillosArreglo.filter(pasta => pasta.getAttribute('data-platillo') === 'pasta');
+    const pizzas = platillosArreglo.filter(pizza => pizza.getAttribute('data-platillo') === 'pizza');
+    const postres = platillosArreglo.filter(postre=> postre.getAttribute('data-platillo') === 'postre');
+
+    mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
+
+}
+
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) =>{
+    btnEnsaladas.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        ensaladas.forEach(ensalada=> contenedorPlatillos.appendChild(ensalada));
+    });
+
+    btnPasta.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+         pastas.forEach(pasta=> contenedorPlatillos.appendChild(pasta));
+    });
+
+    btnPizza.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        pizzas.forEach(pizza=> contenedorPlatillos.appendChild(pizza));
+    });
+    btnPostres.addEventListener('click', ()=>{
+        limpiarHtml(contenedorPlatillos);
+        postres.forEach(postre=> contenedorPlatillos.appendChild(postre));
+    });
+    btnTodos.addEventListener('click',()=>{
+        limpiarHtml(contenedorPlatillos);
+        todos.forEach(todo=> contenedorPlatillos.appendChild(todo));
+    });
+}
+
+const limpiarHtml = (contenedor) =>{
+    while(contenedor.firstChild){
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
